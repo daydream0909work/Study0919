c3f8386a41bfe0b4c1dfb580a3ac5cf6255eb4b3
daydream0909work <43108265+daydream0909work@users.noreply.github.com>
2018-09-30 23:08:57 +0900

    コミット
    指摘をいただいた箇所の修正
    Magicianクラス等を作成(エラー有)

---
+++ Magician.java
@@ -0,0 +1,18 @@
+
+class Magician extends Character3{
+
+	/*
+	public Magician(String name, int hp, int mp, int hit, boolean deadcount, boolean deadflg) {
+		super(name, hp, mp, hit, deadcount, deadflg);
+
+	}
+	*/
+
+	public void introduce() {
+		System.out.println("");
+
+	}
+
+
+
+}
